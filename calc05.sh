556905fe61d99932a8f50d6f54cd08e3a5c6af99
Sakura MATSUBARA <Sakura.Matsubara@sony.com>
2024-09-25 13:50:45 +0900

    フラグを使っていた部分をカウンターを使って修正、引数にファイルが指定されなかった場合の処理を追加

--- calc05.sh
+++ calc05.sh
@@ -6,8 +6,14 @@
 # 結果表示後、スクリプトを終了する。
 #-----------------------------------------------------------------------------------------------
 
-readonly TRUE=0
-readonly FALSE=1
+#引数にファイル名が指定されたかどうか確認する
+#ファイル名が指定されなければexit2でスクリプトを終了する
+function exist_file_name() {
+    if [ $# -eq 0 ]; then
+        echo "ファイル名が指定されませんでした。スクリプトを終了します。"
+        exit 2
+    fi
+}
 
 # 検索したい文字列の入力を受け付ける
 function input_search_text() {
@@ -17,36 +23,39 @@ function input_search_text() {
 }
 
 # メイン処理
+#引数にファイル名が指定されたかどうか確認する
+exist_file_name "$@"
+
 # 引数で受け取った検索対象のファイル名を配列に代入する
 file_name_array=("$@")
 
 # 検索したい文字列の入力を受け付ける
 search_text=$(input_search_text)
 
-# 検索結果を格納するためのフラグ
-is_found=$FALSE
+# 検索対象が見つかったファイルの数を数えるカウンター
+file_counter=0
 
 # ファイルを1行ずつ読み込んで検索を行う
 for file_name in "${file_name_array[@]}"; do
     line_number=0
-    #ファイル名が表示済みかどうか示すフラグ
-    is_displayed=$FALSE
+    #1つのファイルの中で検索対象を含む行が何行見つかったか数えるカウンター
+    line_counter=0
     while read line; do
         line_number=$((line_number + 1))
         if [[ "$line" == *"$search_text"* ]]; then
-            #検索対象の文字列が見つかり、ファイル名が一度も表示されていない時、ファイル名を表示する。
-            if [ $is_displayed = $FALSE ]; then
+            line_counter=$((line_counter + 1))
+            #検索対象を含む行が初めて見つかった場合のみファイル名を表示する
+            if [ $line_counter -eq 1 ]; then
                 echo "ファイル名：""$file_name"
-                is_displayed=$TRUE
             fi
             echo -e "$line_number行目\t$line"
-            is_found=$TRUE
+            file_counter=$((file_counter + 1))
         fi
-    done < "$file_name"
+    done <"$file_name"
 done
 
-# 結果が見つからなかった場合はexit1でスクリプトを終了し、見つかった場合はexit0で終了する。
-if [ $is_found = $FALSE ]; then
+# 検索対象が見つからなかった場合はexit1でスクリプトを終了し、見つかった場合はexit0で終了する。
+if [ $file_counter -eq 0 ]; then
     echo "条件に一致する行がありませんでした。"
     exit 1
 else
